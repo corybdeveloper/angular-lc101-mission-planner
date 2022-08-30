296a85592b8bae84033595fca808b7592fb9d731
Rita Clark-Browning <ritaclark-browning@iMac-607.local>
2022-08-29 21:58:18 -0500

    Started Angular Part 3 Studio

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -21,11 +21,23 @@ export class EquipmentComponent implements OnInit {
    cargoMass: number = 0;
    maximumAllowedMass: number = 2000;
    maxItems: number = 10;
+   
 
    constructor() { }
 
    ngOnInit() { }
 
    // Code your addItem function here:
+
+   addItem(item: object) : boolean {
+    this.cargoHold.push(item);
+    this.cargoMass += item["mass"];
+    if (this.maximumAllowedMass - this.cargoMass <= 200) {
+      return true;
+    } else {
+      return false;
+    }
+   }
+
    
 }
